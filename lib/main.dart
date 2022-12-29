64352d2c60569b7ad953e9c83a89c96ce62ca963
Mudit Rai <raimudit2003@gmail.com>
2022-12-30 01:37:28 +0530

    Step 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_views.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized(); // important
@@ -30,15 +32,16 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // // final emailVerified = user?.emailVerified ?? false; // dont know why does not work well  print(user);
-            // if (user?.emailVerified() ?? false) {
-            //   return const LoginView();
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            // return const Text('Done');
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user == null) {
+              return const LoginView();
+            } else {
+              if (user.emailVerified() == false) {
+                return const VerifyEmailView();
+              } else {
+                return const Text("User is Amazing");
+              }
+            }
           default:
             return const CircularProgressIndicator();
         }
