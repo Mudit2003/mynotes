45fbb2203e2db67c37d92d5c0d5afcd06c41fc2a
Mudit Rai <raimudit2003@gmail.com>
2023-02-10 18:04:39 +0530

    step-13

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -146,7 +146,10 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false);
+    const user = AuthUser(
+      isEmailVerified: false,
+      email: '',
+    );
     _user = user;
     return Future.value(user);
   }
@@ -164,7 +167,10 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true);
+    const newUser = AuthUser(
+      isEmailVerified: true,
+      email: '',
+    );
     _user = newUser;
   }
 }
